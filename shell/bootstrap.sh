025457e482afaf6ff7bb98ac78776d88a82e832f
lamhaison <lamhaison@gmail.com>
2023-06-12 22:29:47 +0700

    [Add] - source lhs tool with specific path instead of using default value

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -10,6 +10,6 @@ export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
 export PATH="$PATH:/opt/homebrew/opt/openvpn/sbin"
 
 # For development
-source /opt/lamhaison-tools/aws-cli-utils/main.sh
-source /opt/lamhaison-tools/helpful-commandlines/main.sh
-source /opt/lamhaison-tools/private-helpful-commandlines/main.sh
+source /opt/lamhaison-tools/aws-cli-utils/main.sh "/opt/lamhaison-tools/aws-cli-utils"
+source /opt/lamhaison-tools/helpful-commandlines/main.sh "/opt/lamhaison-tools/helpful-commandlines"
+source /opt/lamhaison-tools/private-helpful-commandlines/main.sh "/opt/lamhaison-tools/private-helpful-commandlines"
