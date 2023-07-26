8dec3a04265eb901f7f739540b1d519d958c59b9
lamhaison <lamhaison@gmail.com>
2023-07-26 21:35:05 +0700

    [Fix] - moving setting about history to helpful-commandlines

--- bash/settings.bash
+++ bash/settings.bash
@@ -1,6 +1,7 @@
-HISTSIZE=1048576
-HISTFILE="$HOME/.bash_history"
-SAVEHIST=$HISTSIZE
+# HISTSIZE=1048576
+# HISTFILE="$HOME/.bash_history"
+# SAVEHIST=$HISTSIZE
+# Move that setting to helpful-commandlines (main.sh)
 shopt -s histappend # append to history file
 
 export EDITOR=vim
